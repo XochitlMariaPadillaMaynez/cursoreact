56ece59deb93fe30e466d8ce6805eacef7f8af61
PadillaMaynezXochitlMaria <padilla_maynez@hotmail.com>
2021-08-13 12:46:25 -0500

    Primera subida y modificaciones finalesm

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import Button from './Button'
+
+const Header = (props) =>{
+  const onClick = () =>{
+    console.log('Click');
+  }
+
+
+return(
+  <header className='header'>
+  <h1>{props.title}</h1>
+
+  <Button text={props.showAdd ? 'x' : '+'} onClick={props.onAdd} />
+
+  </header>
+)
+}
+
+Header.defaultProps={
+  title:'Título por defecto'
+}
+
+const headingStyle={
+color:'red',
+backgroundColor:'black'
+}
+
+
+export default Header
